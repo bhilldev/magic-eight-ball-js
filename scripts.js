4e07dbb94187fad6a84070607fdc3075f165f2ba
Brandon Hill <bhilldev@outlook.com>
2023-12-14 16:43:25 -0600

    Experimenting with opacity fade in and out effects.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
-"use strict"
+"use strict";
+
+const fortune = ["Maybe...", "It is certain!", "Never", "Ask Again"];
+
+var randomIndex = Math.floor(Math.random() * fortune.length);
+var triangle = document.querySelector(".triangle");
+var text = document.querySelector(".text").textContent;
+console.log(text);
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.visibility = hidden;
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.visibility = visible;
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 50);
+}
+fade(triangle);
