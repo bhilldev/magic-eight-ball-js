24b865ee23f69806a8350fa1942502a343f32b74
Brandon Hill <bhilldev@outlook.com>
2023-12-15 12:12:10 -0600

    Fixed timeout flow. Added onclick to text tag to cycle responses.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
 "use strict";
 
-const fortune = ["Maybe...", "It is certain!", "Never", "Ask Again"];
+const fortune = ["Maybe...", "It is certain!", "Never", "Ask Again..."];
 
-var randomIndex = Math.floor(Math.random() * fortune.length);
 var triangle = document.querySelector(".triangle");
-var text = document.querySelector(".text").textContent;
-console.log(text);
 function fade(element) {
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
         if (op <= 0.1){
             clearInterval(timer);
-            element.style.visibility = hidden;
+            element.style.visibility = "hidden";
+            changeText();
+            unfade(triangle);
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
@@ -21,7 +20,7 @@ function fade(element) {
 
 function unfade(element) {
     var op = 0.1;  // initial opacity
-    element.style.visibility = visible;
+    element.style.visibility = "visible";
     var timer = setInterval(function () {
         if (op >= 1){
             clearInterval(timer);
@@ -31,4 +30,7 @@ function unfade(element) {
         op += op * 0.1;
     }, 50);
 }
-fade(triangle);
+function changeText() {
+    var randomIndex = Math.floor(Math.random() * fortune.length);
+    document.querySelector(".text").textContent = fortune[randomIndex];
+}
