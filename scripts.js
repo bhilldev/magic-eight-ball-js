fc859c74b321d16801b6843c8cd97c3ade3690ea
Brandon Hill <bhilldev@outlook.com>
2024-01-15 17:25:15 -0600

    Click event now disabled until fade in is finished. Lowered HTML indents.

--- scripts.js
+++ scripts.js
@@ -1,36 +1,43 @@
 "use strict";
 
 const fortune = ["Maybe...", "It is certain!", "Never", "Ask Again..."];
-
-var triangle = document.querySelector(".triangle");
+const triangle = document.querySelector('.triangle');
 function fade(element) {
+    document.removeEventListener("click", fade);
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
         if (op <= 0.1){
             clearInterval(timer);
-            element.style.visibility = "hidden";
+            triangle.style.visibility = "hidden";
             changeText();
             unfade(triangle);
         }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        triangle.style.opacity = op;
+        triangle.style.filter = 'alpha(opacity=' + op * 100 + ")";
         op -= op * 0.1;
     }, 50);
+        
+    
 }
 
 function unfade(element) {
+
     var op = 0.1;  // initial opacity
     element.style.visibility = "visible";
     var timer = setInterval(function () {
         if (op >= 1){
-            clearInterval(timer);
+            clearInterval(timer);            
+            document.addEventListener("click", fade);
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
         op += op * 0.1;
     }, 50);
+    
 }
 function changeText() {
     var randomIndex = Math.floor(Math.random() * fortune.length);
-    document.querySelector(".text").textContent = fortune[randomIndex];
+    document.querySelector('.text').textContent = fortune[randomIndex];
 }
+
+document.addEventListener("click", fade);
